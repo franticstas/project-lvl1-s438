ba47a2dd171093e6a39872def30418498a3d9ecb
webstas <webstas86@gmail.com>
2019-02-05 14:44:08 +0300

    corrected the mentor's comments on the fourth step

--- src/games/even.js
+++ src/games/even.js
@@ -1,27 +1,23 @@
 import readlineSync from 'readline-sync';
 
-const isEven = number => (number % 2 ? 'no' : 'yes');
-const randomNumber = () => {
-  const min = 1;
-  const max = 100;
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-};
+const isEven = number => number % 2;
+const randomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
 
-const setQuestions = (count = 0) => {
-  if (count === 3) {
+const generateQuestions = (questionNumber = 0) => {
+  if (questionNumber === 3) {
     return true;
   }
 
-  const rNum = randomNumber();
+  const question = randomNumber(1, 99);
 
-  const correctAnswer = isEven(rNum);
+  const correctAnswer = !isEven(question) ? 'yes' : 'no';
 
-  console.log(`Question: ${rNum}`);
+  console.log(`Question: ${question}`);
 
   const answer = readlineSync.question('Your answer: ');
   if (correctAnswer === answer) {
     console.log('Correct!');
-    return setQuestions(count + 1);
+    return generateQuestions(questionNumber + 1);
   }
 
   console.log(`${answer} is wrong answer ;(. Correct answer was ${correctAnswer}.`);
@@ -31,14 +27,8 @@ const setQuestions = (count = 0) => {
 export default () => {
   console.log('Welcome to the Brain Games!');
   console.log('Answer "yes" if number even otherwise answer "no".\n');
-  const namePlayer = readlineSync.question('May I have your name? : ');
+  const playerName = readlineSync.question('May I have your name? : ');
   let message = '';
-
-  if (setQuestions()) {
-    message = `Congratulations, ${namePlayer}`;
-  } else {
-    message = `Let's try again, ${namePlayer}`;
-  }
-
+  message = generateQuestions() ? `Congratulations, ${playerName}` : `Let's try again, ${playerName}`;
   console.log(message);
 };
