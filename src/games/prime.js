9e2421e944e69f187690537e9483c0c8dfa6a9d6
webstas <webstas86@gmail.com>
2019-02-07 14:03:29 +0300

    fixed remarks on step 8

--- src/games/prime.js
+++ src/games/prime.js
@@ -3,15 +3,19 @@ import { cons } from 'hexlet-pairs';
 import randomNumber from '../utils';
 
 const gameDescription = 'Answer "yes" if given number is prime. Otherwise answer "no".';
-const isPrime = (question, count = 2) => {
-  if (count === question) {
-    return true;
-  }
-  return question % count === 0 ? false : isPrime(question, count + 1);
+const isPrime = (question) => {
+  const iter = (count = 2) => {
+    if (count === question) {
+      return true;
+    }
+    return question % count === 0 ? false : iter(count + 1);
+  };
+
+  return question < 2 ? false : iter();
 };
 
 const generateQuestionAndAnswer = () => {
-  const question = randomNumber(1, 20);
+  const question = randomNumber(-10, 10);
   const correctAnswer = isPrime(question) ? 'yes' : 'no';
 
   return cons(question, correctAnswer);
